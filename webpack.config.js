50fd95a0acbfb0b7c0686ab189243710444c3850
Joseph Quispe <josepheqch@gmail.com>
2019-09-19 23:03:20 -0500

    configuring webpack... webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname,'dist/js'),
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/bundle.js'
+    },
     //para webpack config
     //mode: 'development' o mode: production
     //para el archivo npm  package.json
     //"dev": "webpack --mode development",
     //"build": "webpack --mode production"
-}
+    devServer: {
+        contentBase: './dist'
+        //clientLogLevel: 'silent' //para desahabilitar el log
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+    })]
+};
