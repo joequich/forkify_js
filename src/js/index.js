8aba71e5eb2798a65f4012db4b757c4d3082f089
Joseph Quispe <josepheqch@gmail.com>
2019-09-28 21:08:10 -0500

    building the search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search'
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 /** Global state of the app
  * - search object
  * - current recipe object
@@ -13,26 +14,33 @@ const state = {}
 
 const controlSearch = async () => {
     // 1ero get query from view
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if(query) {
         // 2do new search object and add to state
         state.search = new Search(query);
 
         // 3ero prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
+        console.time()
         // 4to search for recipes
         await state.search.getResults();
 
         // 5to render results on UI
         console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+        
+        console.timeEnd()
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
 
+
+console.log("Hola");
 //https://www.food2fork.com/api/search
 //aa6634f7387fbbe73d4722f26b2fe165 
