a3cfaf795b3681841822dac64fe9950026e06176
Joseph Quispe <josepheqch@gmail.com>
2019-10-02 22:08:30 -0500

    building recipe view part 2

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /** Global state of the app
  * - search object
@@ -64,6 +65,10 @@ const controlRecipe = async () => {
     const id = window.location.hash.substring(1);
     if (id) {
         // prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        //highlight
+        searchView.highlightSelected(id);
 
         // create new recipe object
         state.recipe = new Recipe(id);
@@ -76,6 +81,8 @@ const controlRecipe = async () => {
             state.recipe.calcTime();
             state.recipe.calcServings();
             // render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
             console.log(state.recipe);
 
         } catch (error) {
